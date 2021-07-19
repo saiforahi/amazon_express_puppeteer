342bd9ad0a2c62ca2f6cf61a08a9b080fde726a7
Shaif Azad <shaif.rahi@northsouth.edu>
2021-07-19 09:53:47 +0600

    returning responses

--- scrapper.js
+++ scrapper.js
@@ -2,6 +2,7 @@ const schedule = require('./plateform/amazon');
 //const schedule = require('./orderSchedule')
 //const test = require('./test');
 const scrapper = async (amazon_buyer_account) => {
-    schedule(amazon_buyer_account);
+    const result = schedule(amazon_buyer_account);
+    return result
 }
 module.exports = scrapper;
